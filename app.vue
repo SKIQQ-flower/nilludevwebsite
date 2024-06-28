05e521cf92873022f1dd41fb905d279550c856c4
skiqq <chibifoxdiscord@gmail.com>
2024-06-28 01:13:49 -0300

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <Link rel="preconnect" href="https://fonts.googleapis.com"/>
+  <Link rel="preconnect" href="https://fonts.gstatic.com"/>
+  <Link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet"/>
+  <NuxtPage />
+</template>
+
+<style>
+#__nuxt {
+  @apply flex flex-col
+}
+</style>
