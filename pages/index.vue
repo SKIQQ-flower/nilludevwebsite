79d48adceb90aa56b6652762b7612fae81687d68
skiqq <chibifoxdiscord@gmail.com>
2024-06-28 10:35:49 -0300

    Fix resolution

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,20 @@
 <template>
     <div class="h-dvh absolute flex flex-col items-center">
-        <div class="-rotate-[46deg] absolute w-[50rem] h-[90rem] -top-[4rem] pt-5 pl-10 -left-[3rem] inline-flex justify-end bg-white">
+        <div class="-rotate-[45deg] w-[180%] h-[170%] fixed right-[40%] -bottom-[160%] inline-flex justify-end bg-white">
             <div
-                class="text-pink-500 h-fit w-92 gap-2 flex items-end flex-col *:p-5 *:transition-all px-5 py-40 font-extrabold font-montserrat text-5xl">
-                <button ref="aboutBtn" @mouseover="showTooltip('about')" @mouseout="hideTooltip"
+                class="text-pink-500 h-fit w-92 gap-2 flex items-end flex-col *:p-5 *:transition-all px-5 pt-[15%] font-extrabold font-montserrat text-5xl">
+                <button ref="aboutBtn"
                     class="hover:bg-pink-500 hover:text-white uppercase shadow hover:shadow-lg active:translate-x-5 active:-translate-y-5">About
                     me</button>
-                <button ref="socialBtn" @mouseover="showTooltip('social')" @mouseout="hideTooltip"
+                <button ref="socialBtn"
                     class="hover:bg-pink-500 hover:text-white uppercase shadow hover:shadow-lg active:translate-x-5 active:-translate-y-5">Social
                     Media</button>
-                <button ref="blogBtn" @mouseover="showTooltip('blog')" @mouseout="hideTooltip"
+                <button ref="blogBtn"
                     class="hover:bg-pink-500 hover:text-white uppercase shadow hover:shadow-lg active:translate-x-5 active:-translate-y-5">Blog</button>
             </div>
         </div>
     </div>
 </template>
-
-<script>
-import { ref, computed, nextTick } from 'vue';
-
-export default {
-    setup() {
-        const tooltip = ref('');
-        const tooltipStyles = ref({});
-        const aboutBtn = ref(null);
-        const socialBtn = ref(null);
-        const blogBtn = ref(null);
-
-        const showTooltip = async (type) => {
-            tooltip.value = type;
-            await nextTick(); // Espera a DOM atualizar
-            const btn = type === 'about' ? aboutBtn.value : type === 'social' ? socialBtn.value : blogBtn.value;
-            const rect = btn.getBoundingClientRect();
-            tooltipStyles.value = {
-                top: `${rect.top + window.scrollY + btn.offsetHeight / 9}px`,
-                left: `${rect.left + window.scrollX + btn.offsetWidth + 1000}px`,
-            };
-        };
-
-        const hideTooltip = () => {
-            tooltip.value = '';
-        };
-
-        return {
-            tooltip,
-            tooltipStyles,
-            aboutBtn,
-            socialBtn,
-            blogBtn,
-            showTooltip,
-            hideTooltip,
-        };
-    },
-};
-</script>
 
 <style>
 html {
